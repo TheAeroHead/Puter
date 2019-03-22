91aa0132802279231f2c207485fbc4266d5b0ea1
Benjamin Koch <bjckoch@gmail.com>
2019-03-22 18:34:49 -0500

    Updated to textfield to get around charfield max limit.

--- products/models.py
+++ products/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
 	name = models.CharField(max_length=200)
-	description = models.CharField(max_length=255) # will need to use some other model type to increase size (charfield max is like 256 I think)
+	description = models.TextField()
 	shipping_speed = models.IntegerField(default=3)
 	price = models.DecimalField(max_digits=6, decimal_places=2)
-	category = models.CharField(max_length=200) # may need some sort of array for category if we want to allow multiple categorizations
+	category = models.ForeignKey(Category, on_delete=models.CASCADE) #ForeignKey field allows us to specify another class as a type
+
+class Category(models.Model):
+	name = models.CharField(max_length=200) # may need some sort of array for category if we want to allow multiple categorizations
